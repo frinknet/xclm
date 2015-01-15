d4a2c1318f8d36589191b55c7607a2ebfff49aeb
Rodney Giles <rodney@rodneygiles.net>
2015-01-14 20:04:39 -0700

    added window-focus

--- window-focus.c
+++ window-focus.c
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <err.h>
 #include <xcb/xcb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
 
-#include "xcbh.h"
+#include "util.h"
 
 static xcb_connection_t *conn;
 
@@ -20,10 +21,11 @@ main(int argc, char **argv)
 
 	xcbh_conn_init(&conn);
 
-	while (*argv) {
-		win = strtoul(*argv++, NULL, 16);
+	win = strtoul(argv[1], NULL, 16);
 
-		xcb_map_window(conn, win);
+	if (xcbh_win_exists(conn, win)) {
+		xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, win,
+				XCB_CURRENT_TIME);
 	}
 
 	xcbh_conn_kill(&conn);
