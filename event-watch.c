adbce5864cc6470865f9b629ec099b74e593c9ee
Micon Frink <frink@frinknet.com>
2016-07-24 04:12:28 -0500

    adding focus borders and simplifying event loop

--- event-watch.c
+++ event-watch.c
@@ -3,8 +3,7 @@
 #include "xcmd.h"
 
 xcmd_windows (3, "event-dir") {
-	//xcb_window_t *child;
-
+	char *event_dir = xcmd_next;
 	uint32_t mask = XCB_EVENT_MASK_NO_EVENT
 		| XCB_EVENT_MASK_STRUCTURE_NOTIFY
 		| XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
@@ -33,18 +32,5 @@ xcmd_windows (3, "event-dir") {
 		| XCB_EVENT_MASK_OWNER_GRAB_BUTTON
 		;
 
-	char *event_dir = xcmd_next;
-
-	xcmd_win_loop {
-		// xmpl_event_register(xcmd_conn, xcmd_win, mask);
-		// xmpl_window_list_children(xcmd_conn, xcmd_win, &child);
-
-		// while (*child) {
-		// 	xmpl_event_register(xcmd_conn, *child++, mask);
-		// }
-
-		xmpl_event_loop(xcmd_conn, xcmd_win, event_dir, mask);
-	}
-
-	xcmd_exit(0);
+	xcmd_win_exec(xmpl_event_loop(xcmd_conn, xcmd_win, event_dir, mask));
 }
